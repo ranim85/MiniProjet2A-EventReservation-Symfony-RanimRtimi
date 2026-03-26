7d474af49a5ea4fbfc1133024d125b9a8617eae4
Ranim Rtimi <rtimiranim80@gmail.com>
2026-03-26 23:38:21 +0100

    feat: configuration JWT (LexikJWT + refresh tokens) + endpoint register

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/api/register', name: 'api_register', methods: ['POST'])]
+    public function register(
+        Request $request,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $hasher,
+        JWTTokenManagerInterface $jwtManager
+    ): JsonResponse {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['username'], $data['password'])) {
+            return $this->json(['error' => 'Données manquantes'], 400);
+        }
+
+        $user = new User();
+        $user->setUsername($data['username']);
+        $user->setPasswordHash($hasher->hashPassword($user, $data['password']));
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json([
+            'message' => 'Utilisateur créé',
+            'token' => $jwtManager->create($user),
+        ], 201);
+    }
+
+    #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
+    public function login(): Response
+    {
+        return $this->render('security/login.html.twig');
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void {}
+}
